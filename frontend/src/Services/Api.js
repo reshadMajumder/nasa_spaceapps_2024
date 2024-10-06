7694d7bd77f95eb469025f4d7dc323e23a9616a6
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-06 19:36:42 +0600

    in frontend vercel api link added

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,4 +1,5 @@
-const B_URL = 'https://nasaspaceapps2024-production.up.railway.app';
+// const B_URL = 'https://nasaspaceapps2024-production.up.railway.app';
+const B_URL = 'https://nasa-spaceapps-2024-1dqy.vercel.app';
 // const B_URL = 'http://127.0.0.1:8000';
 
 export default B_URL;
