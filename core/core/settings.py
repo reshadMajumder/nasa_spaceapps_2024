95c71ac899b566c80cd9d5f6028f1ff9d9142a40
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2024-10-18 22:58:09 +0600

    updated CORS allowed origin
    added another origin of backend hosted in aws

--- core/core/settings.py
+++ core/core/settings.py
@@ -149,7 +149,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'https://slingringui-production.up.railway.app',
-    "https://nasaspaceapps2024-production-6dc8.up.railway.app"
+    "https://nasaspaceapps2024-production-6dc8.up.railway.app",
+    "https://rexhad.co"
 
 ]
 
--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,4 +1,4 @@
 const B_URL = 'https://nasaspaceapps2024-production.up.railway.app';
-// const B_URL = 'http://127.0.0.1:8000';
+// const B_URL = 'https://rexhad.co';
 
 export default B_URL;
